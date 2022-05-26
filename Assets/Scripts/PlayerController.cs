222303d4a9c391931b29ef17f25cce1a7bf5e39a
Owen <105467906+OwenPallister@users.noreply.github.com>
2022-05-26 17:11:20 +1000

    Creation of the Player controller script and added the player moving forward momentum and allowing the player rotation to be controlled with wasd. Also added a sphere to the player character to be able to determine which way the player is looking.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    private Vector3 _input;
+
+
+    void Update()
+    {
+
+        GatherInput();
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+
+        Move();
+
+    }
+
+    void GatherInput()
+    {
+        
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+    }
+
+
+    void Look()
+    {
+
+        var relative = (transform.position + _input) - transform.position;
+        var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+        transform.rotation = rot;
+    }
+    void Move()
+    {
+        _rb.MovePosition(transform.position + transform.forward *_speed *Time.deltaTime);
+
+    }
+}
